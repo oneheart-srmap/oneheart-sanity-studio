354eeb3abe34285e1f97096c166b80910ef2f86f
dave1725 <davedmj1725@gmail.com>
2025-10-25 00:42:56 +0530

    config: add app id

--- sanity.cli.js
+++ sanity.cli.js
@@ -6,6 +6,7 @@ export default defineCliConfig({
     dataset: 'production'
   },
   deployment: {
+    appId: "n65b6dete9by6ochf0xjgmbh",
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/cli#auto-updates
