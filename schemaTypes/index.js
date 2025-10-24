280c9376331256d7c179640232031759f0b7bbd9
dave1725 <davedmj1725@gmail.com>
2025-10-25 00:42:31 +0530

    chore: modify main index.js

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -1,1 +1,5 @@
-export const schemaTypes = []
+import { eventType } from "./eventType";
+import { postType } from "./postType";
+import { authorType } from "./author";
+
+export const schemaTypes = [postType, eventType, authorType]
